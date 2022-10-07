925394f7fafd4079d3f55d700f83d90b67c32d27
Spencer Christian <s.christian141@gmail.com>
2022-10-07 13:39:16 -0400

    AddCharacterToRow Add Character to Rank Row

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./styles.css";
-var userSelection = "";
+var selectedCharacter = "";
 var characterList = [
   "batman",
   "finn",
@@ -20,13 +20,19 @@ var characterList = [
   "iron_giant"
 ];
 
-var rankingOrder = [
-  { S: ["batman", "finn", "superman", "bugs"] },
-  { A: ["shaggy", "jake", "lebron", "harley"] },
-  { B: ["taz", "arya", "steven", "velma", "reindog"] },
-  { C: ["garnet", "wonder_woman", "tom_and_jerry"] },
-  { D: ["iron_giant"] }
-];
+var rankingOrder = [{ S: [] }, { A: [] }, { B: [] }, { C: [] }, { D: [] }];
+
+function clg(content) {
+  console.log(`${content}`, content);
+}
+
+// var rankingOrder = [
+//   { S: ["batman", "finn", "superman", "bugs"] },
+//   { A: ["shaggy", "jake", "lebron", "harley"] },
+//   { B: ["taz", "arya", "steven", "velma", "reindog"] },
+//   { C: ["garnet", "wonder_woman", "tom_and_jerry"] },
+//   { D: ["iron_giant"] }
+// ];
 
 const inputField = document.getElementById("input-field-character");
 
@@ -73,13 +79,14 @@ const rankingOrderKeys = rankingOrder.map((item) => Object.keys(item)[0]);
  */
 // const Add New Character = document.getElementsByClassName("btn-new");
 // const tierRow = document.getElementById("tier-row");
+// const rank_selection_list = document.querySelectorAll(".ranking-key-text");
+
 const tier_row_container = document.getElementById("tier-row-container");
 const tier_row_full_list = document.getElementById("tier-row-full-list");
-const characterIcon = document.querySelectorAll(".characterIcon");
-const tier_selection_container = document.getElementById(
-  "tier-selection-container"
+const tier_modal_selection_container = document.getElementById(
+  "tier-modal-selection-container"
 );
-const rank_selection_list = document.querySelectorAll(".ranking-key-text");
+// const characterIcon = document.querySelectorAll(".characterIcon");
 
 // array.forEach(element => {
 
@@ -91,7 +98,7 @@ const rank_selection_list = document.querySelectorAll(".ranking-key-text");
 
 // })
 
-let tierSelectRow = () => {
+function tierModalSelectRow() {
   let icons = rankingOrderKeys.map((rankKey) => {
     return `
     <div class="ranking-key-text" dataValue="${rankKey}" >
@@ -102,27 +109,29 @@ let tierSelectRow = () => {
     .join(",")
     .replace(/,/g, "")}</div>`;
   return tierRow;
-};
+}
 
-let rank = rankingOrder.map((rank) => {
-  let key = Object.keys(rank);
-  let images = rank[key].map((img, index) => {
-    return `<img class="avatar characterRemoveIcon" charKey="${index}" src="./image/${img}.webp" dataValue="${img}" alt="${img}">`;
+function renderRankingOrder(content) {
+  let rank = content.map((rank) => {
+    let key = Object.keys(rank);
+    let images = rank[key].map((img, index) => {
+      return `<img class="avatar characterRemoveIcon" charKey="${index}" src="./image/${img}.webp" dataValue="${img}" alt="${img}">`;
+    });
+    return ` 
+    <div class="tier-row">
+      <div class="tier-heading-wrapper"><p class="tier-heading">${key}:</p></div>
+      <div class="tier-body avatar-wrapper">
+        ${images.join(",").replace(/,/g, "")}
+      </div>
+    </div>`;
   });
-  return ` 
-  <div class="tier-row">
-    <div class="tier-heading-wrapper"><p class="tier-heading">${key}:</p></div>
-    <div class="tier-body avatar-wrapper">
-      ${images.join(",").replace(/,/g, "")}
-    </div>
-  </div>`;
-});
+  return rank;
+}
 
-let characterRankDiv = () => {
+function renderCharacterRankList2() {
   let images = characterList.map((img, index) => {
     return `<img class="characterIcon" src="./image/${img}.webp" dataValue="${img}" alt="${img}">`;
   });
-
   return [
     `<div class=" tier-full-list">
     <div class="tier-heading-wrapper"><p class="tier-heading"> List: </p></div>
@@ -131,51 +140,105 @@ let characterRankDiv = () => {
     </div>
   </div>`
   ];
-};
+}
+function renderCharacterRankList(content) {
+  let characterImage = content.map((img, index) => {
+    return `<img class="characterIcon" src="./image/${img}.webp" dataValue="${img}" alt="${img}">`;
+  });
+  let characterDivArray = [
+    `<div class=" tier-full-list">
+    <div class="tier-heading-wrapper"><p class="tier-heading"> List: </p></div>
+    <div class="tier-full-body avatar-wrapper">
+      ${characterImage.join(",").replace(/,/g, "")}
+    </div>
+  </div>`
+  ];
+  return characterDivArray;
+}
 /* Content Has Loaded Render Table */
 
-tier_row_container.innerHTML = rank.join(",").replace(/,/g, "");
-tier_row_full_list.innerHTML = characterRankDiv();
-tier_selection_container.innerHTML = tierSelectRow();
+function updateTierRowContainer(content) {
+  tier_row_container.innerHTML = content.join(",").replace(/,/g, "");
+  console.log(tier_row_container);
+}
+function updateCharacterRankList(content) {
+  tier_row_full_list.innerHTML = content;
+}
+// tier_row_full_list.innerHTML = renderCharacterRankList();
+tier_modal_selection_container.innerHTML = tierModalSelectRow();
 
-// tier_row_full_list.innerHTML = characterRankDiv()
-window.addEventListener("DOMContentLoaded", (event) => {
-  console.log("DOM fully loaded and parsed");
-});
+const tier_row_full_list_CharacterList = document.querySelectorAll(
+  ".characterIcon"
+);
+const ranking_key_text = document.querySelectorAll(".ranking-key-text");
 
 /*-------------------
  * Event Listeners
  -------------------*/
 
 // Event - Display Modal
-inputField.addEventListener("input", (event) => {
-  // modal.style.display = "block";
-  searchCharacter(characterList, event.target.value);
-});
-// Event - Search Character in Modal
-inputField.addEventListener("keypress", (event) => {
-  if (event.key === "Enter") {
-    console.log(event.key);
-    console.log(event.target.value);
-  }
-});
-
-const tier_row_full_list_CharacterList = document.querySelectorAll(
-  ".characterIcon"
-);
-
-for (let i = 0; i < tier_row_full_list_CharacterList.length; i++) {
-  tier_row_full_list_CharacterList[i].addEventListener("click", (event) => {
-    modal.style.display = "block";
-    console.log(event.target.attributes["datavalue"].value);
-    // userSelection = event.target.attributes["datavalue"].value;
-    // for (let y = 0; y < rank_selection_list.length; y++) {
-    //   rank_selection_list[y].addEventListener("click", (event) => {
-    //     console.log(event.target.attributes["datavalue"].value);
-    //   });
-    // }
+function handleDisplayModal() {
+  inputField.addEventListener("input", (event) => {
+    // modal.style.display = "block";
+    searchCharacter(characterList, event.target.value);
   });
 }
+// Event - Search Character in Modal
+function handleSearchCharacterModal() {
+  inputField.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+      console.log(event.key);
+      console.log(event.target.value);
+    }
+  });
+}
+
+function modalDisplayEventListner() {
+  console.log("trigger modal display");
+  let tierFullList = document.querySelectorAll(".characterIcon");
+  for (let i = 0; i < tierFullList.length; i++) {
+    debugger;
+    tierFullList[i].addEventListener("click", (event) => {
+      modal.style.display = "block";
+      console.log(event.target.attributes["datavalue"].value);
+      selectedCharacter = event.target.attributes["datavalue"].value;
+    });
+  }
+}
+
+function AddCharacterToRow(rank_Key, character) {
+  for (let i = 0; i < rankingOrder.length; i++) {
+    if (rankingOrder[i][rank_Key] !== undefined) {
+      rankingOrder[i][rank_Key].push(character);
+    }
+  }
+  updateTierRowContainer(renderRankingOrder(rankingOrder));
+}
+
+function removeFromCharacterList(content) {
+  const findCharacter = (element) => element === content;
+  let matchCharacterIndex = characterList.findIndex(findCharacter);
+  characterList.splice(matchCharacterIndex, 1);
+  updateCharacterRankList(renderCharacterRankList(characterList));
+  modalDisplayEventListner();
+}
+function handlAddCharacterRow() {
+  for (let i = 0; i < ranking_key_text.length; i++) {
+    ranking_key_text[i].addEventListener("click", (event) => {
+      let rank = event.target.attributes["datavalue"].value;
+      modal.style.display = "none";
+      AddCharacterToRow(rank, selectedCharacter);
+      removeFromCharacterList(selectedCharacter);
+    });
+  }
+}
+
+updateTierRowContainer(renderRankingOrder(rankingOrder));
+updateCharacterRankList(renderCharacterRankList(characterList));
+handleSearchCharacterModal();
+modalDisplayEventListner();
+handleDisplayModal();
+handlAddCharacterRow();
 
 span.onclick = function () {
   modal.style.display = "none";
@@ -186,7 +249,3 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 };
-// tier_row_full_list_CharacterList.map((item) =>{
-//
-// })
-console.log("user selection: ", userSelection);
